1a92fa0d63c3e5dbcf519b2b4abd48e1582d2825
Deb Taylor <deb.taylor@intel.com>
2020-04-13 19:49:15 -0400

    Items created/updated to correct tech writer's build
    Signed-off-by: Deb Taylor <deb.taylor@intel.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Sphinx GUI Utility',
+      version='0.1',
+      description='Build Sphinx docs from a GUI',
+      author='Deb Taylor',
+      author_email='deb.taylor@intel.com',
+      url='https://github.com/deb-intel/sof-docs',
+      )
