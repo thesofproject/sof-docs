4858f01bdc74beab21297ec94c2b8e85fdaeed9a
Deb Taylor <deb.taylor@intel.com>
2020-04-14 12:41:34 -0400

    Correcting author errors
    Signed-off-by: Deb Taylor <deb.taylor@intel.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='Sphinx GUI Utility',
       version='0.1',
       description='Build Sphinx docs from a GUI',
-      author='Kevin Putnam',
-      author_email='kevin.putnam@intel.com',
-      url='https://gitlab.devtools.intel.com/kputnam/doc-gui/',
+      author='Deb Taylor',
+      author_email='deb.taylor@intel.com',
+      url='https://github.com/deb-intel/sof-docs'
       )
