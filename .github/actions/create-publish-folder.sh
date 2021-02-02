74066cb4f7c4abf41480d7ca760b1a3f0e8ca5a5
Pan Xiuli <xiuli.pan@linux.intel.com>
2021-02-02 03:35:45 +0000

    CI: GitHub: add local action to create publish folder
    use local action to create publish folder for local test
    
    Signed-off-by: Pan Xiuli <xiuli.pan@linux.intel.com>

---
+++ .github/actions/create-publish-folder.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# create publish folder for github.io
+mkdir _build/latest
+mv -v _build/html/* _build/latest
+mv _build/latest _build/html/
+touch _build/html/.nojekyll
+mv scripts/publish-README.md _build/html/README.md
+mv scripts/publish-index.html _build/html/index.html
