2371a1d23a5ef73984595de1f8b2843d9f1e490f
Michal Wasko <michal.wasko@intel.com>
2022-04-06 14:34:51 +0200

    sphinx blockdiag support
    sphinxcontrib-blockdiag extension added to
    generate diagrams from the code
    
    Signed-off-by: Michal Wasko <michal.wasko@intel.com>

--- conf.py
+++ conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['breathe', 'sphinx.ext.graphviz', 'sphinxcontrib.plantuml',
-              'sphinx.ext.todo', 'sphinx.ext.extlinks',
+              'sphinx.ext.todo', 'sphinx.ext.extlinks', 'sphinxcontrib.blockdiag'
 ]
 
 
